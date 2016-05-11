a866b704f3aa937b849f57a8ba08dbed3c68fe9f
Andrey Antukh <niwi@niwi.nz>
2016-05-11 19:52:21 +0300

    Set version to 0.2.0-SNAPSHOT.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject funcool/struct "0.1.0"
+(defproject funcool/struct "0.2.0-SNAPSHOT"
   :description "A structural validation library for Clojure(Script)"
   :url "https://github.com/funcool/struct"
   :license {:name "Public Domain" :url "http://unlicense.org/"}
