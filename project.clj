4fed6dfccc6931ded2bc899a6e97be29300e6d03
Andrey Antukh <niwi@niwi.nz>
2016-05-11 19:52:14 +0300

    Update dependencies.

--- project.clj
+++ project.clj
@@ -3,12 +3,12 @@
   :url "https://github.com/funcool/struct"
   :license {:name "Public Domain" :url "http://unlicense.org/"}
   :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]
-                 [org.clojure/clojurescript "1.8.40" :scope "provided"]
+                 [org.clojure/clojurescript "1.8.51" :scope "provided"]
                  [funcool/cuerdas "0.7.2"]]
   :source-paths ["src"]
   :test-paths ["test"]
   :codeina {:sources ["src"]
             :reader :clojure
             :target "doc/dist/latest/api"}
-  :plugins [[funcool/codeina "0.3.0"]
-            [lein-ancient "0.6.7" :exclusions [org.clojure/tools.reader]]])
+  :plugins [[funcool/codeina "0.4.0"]
+            [lein-ancient "0.6.10" :exclusions [org.clojure/tools.reader]]])
