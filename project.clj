c0ac0741a2c65bd52d485415255f66e142b32810
Andrey Antukh <niwi@niwi.nz>
2018-01-11 11:32:50 +0100

    Update dependencies.

--- project.clj
+++ project.clj
@@ -2,13 +2,13 @@
   :description "A structural validation library for Clojure(Script)"
   :url "https://github.com/funcool/struct"
   :license {:name "Public Domain" :url "http://unlicense.org/"}
-  :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]
-                 [org.clojure/clojurescript "1.9.854" :scope "provided"]
-                 [funcool/cuerdas "2.0.4"]]
+  :dependencies [[org.clojure/clojure "1.9.0" :scope "provided"]
+                 [org.clojure/clojurescript "1.9.946" :scope "provided"]
+                 [funcool/cuerdas "2.0.5"]]
   :source-paths ["src"]
   :test-paths ["test"]
   :codeina {:sources ["src"]
             :reader :clojure
             :target "doc/dist/latest/api"}
-  :plugins [[funcool/codeina "0.4.0"]
-            [lein-ancient "0.6.10" :exclusions [org.clojure/tools.reader]]])
+  :plugins [[funcool/codeina "0.5.0"]
+            [lein-ancient "0.6.15" :exclusions [org.clojure/tools.reader]]])
